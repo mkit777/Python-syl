f48a4426b6130ca50fc995bd6e5c78ba019c73a4
zy-668 <zy-668@qq.com>
2018-02-06 23:42:05 +0800

    finish challenge08 and clear some file

---
+++ challenge-08/news/app.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+from flask import Flask,render_template,request,make_response,url_for,redirect,abort
+import json,os
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from pymongo import MongoClient
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root@localhost/shiyanlou'
+db = SQLAlchemy(app)
+client = MongoClient('127.0.0.1',27017)
+collection  = client.syl.doc
+class File(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(80))
+    create_time = db.Column(db.DateTime)
+    category_id = db.Column(db.Integer,db.ForeignKey('category.id'))
+    content = db.Column(db.Text)
+    
+    category = db.relationship('Category',backref=db.backref('files',lazy='dynamic'))
+
+
+    def __init__(self,title,category,content,create_time=None):
+        self.title = title
+        # self.category_id = category_id
+        self.category = category
+        self.content = content
+        if create_time is None:
+            self.create_time = datetime.now()
+    
+    def __repr__(self):
+        return '<File title:{}>'.format(self.title)
+   
+    def exists(self):
+        if collection.find_one(self.mongokey) is None:
+            return False
+        return True
+
+    def add_tag(self,tag_name):
+        if not self.exists():
+            collection.insert_one({'fileid':self.id,'tags':[tag_name]})
+        else:
+            tags =collection.find_one(self.mongokey).get('tags')
+            tags.append(tag_name)
+            collection.update(self.mongokey,{'$set':{'tags':tags}})
+    
+    @property
+    def mongokey(self):
+        return {'fileid':self.id} 
+
+    def remove_tag(self,tag_name):
+        tags = collection.find_one(self.mongokey).get('tags')
+        try:
+            tags.remove(tag_name)
+        except IndexEorror:
+            print('tag_name is None')
+            return
+        doc.update(self.mongokey,{'$set':{'tags':tags}})
+    
+    def speak(self):
+        print('hello')
+    @property
+    def tags(self):
+        return collection.find_one(self.mongokey).get('tags')
+
+class Category(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(80))
+
+
+    def __init__(self,name):
+        self.name =name
+
+    def __repr__(self):
+        return '<Category {}>'.format(self.name)
+
+    
+
+@app.route('/')
+def index():
+    return render_template('index.html',artical_list = db.session.query(File).all())
+
+@app.route('/files/<fileid>')
+def file(fileid):
+    file = db.session.query(File).filter(File.id==fileid).first()
+    if file is None:
+        abort(404)
+    category = db.session.query(Category).filter(Category.id==file.category_id).first()
+    return render_template('file.html',file=file,category=category.name)
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html',message='shiyanlou 404'),404
+
+def init_data():
+    db.create_all()
+    java = Category('Java')
+    python = Category('Python')
+    file1 = File('Hello Java',java,'File Content - Java is cool!')
+    file2 = File('Hello Python',python,'File Content - Python is cool!')
+    db.session.add(java)
+    db.session.add(python)
+    db.session.add(file1)
+    db.session.add(file2)
+    db.session.commit()
+
+    file1.add_tag('tech')
+    file1.add_tag('java')
+    file1.add_tag('linux')
+    
+    file2.add_tag('tech')
+    file2.add_tag('python')
+
+
+if __name__=='__main__':
+    pass
